ea771e8807e633784dc8ca0e6e87696d9e34f96b
AtacanUtkusavas <atacan.utkusavas@hotmail.com>
2023-12-27 02:30:12 +0300

    Recipe suggestion google cloud function is connected with mobile

--- bastirchef/lib/pages/homepage.dart
+++ bastirchef/lib/pages/homepage.dart
@@ -4,13 +4,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:bastirchef/firebase_options.dart';
 import 'package:bastirchef/pages/src/food_box.dart';
+import 'package:bastirchef/services/suggestion_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'src/drop_down_text_field.dart';
 
-
 class Homepage extends StatefulWidget {
-
   @override
   State<Homepage> createState() => _HomepageState();
 }
@@ -18,6 +17,7 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   var userData = {};
   List<Map<String, dynamic>> allIngredients = [];
+  List<dynamic> suggestedRecipeIds = [];
   bool isLoading = false;
 
   @override
@@ -39,10 +39,11 @@ class _HomepageState extends State<Homepage> {
       userData = userSnap.data()!;
       print(userData);
 
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-        .collection('ingredients').get();
-      allIngredients = querySnapshot.docs.map((DocumentSnapshot doc) =>
-        doc.data() as Map<String, dynamic>).toList();
+      QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection('ingredients').get();
+      allIngredients = querySnapshot.docs
+          .map((DocumentSnapshot doc) => doc.data() as Map<String, dynamic>)
+          .toList();
       allIngredients.forEach((doc) {
         print(doc);
       });
@@ -53,8 +54,27 @@ class _HomepageState extends State<Homepage> {
     setState(() {
       isLoading = false;
     });
+    await getRecipeSuggestions();
   }
 
+  Future<void> getRecipeSuggestions() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final service = SuggestionService();
+      const userId = "RietHUeDDXWyYFmhJTzCGP4B7gQ2";
+      final recipeIds = await service.makeSuggestion(userId);
+      setState(() {
+        suggestedRecipeIds = recipeIds;
+      });
+    } catch (e) {
+      print('Error fetching recipe suggestions: $e');
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,79 +83,90 @@ class _HomepageState extends State<Homepage> {
       home: Scaffold(
           backgroundColor: Color(0xFFE3E3E3),
           body: SingleChildScrollView(
+              child: Column(children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.32,
+              width: MediaQuery.of(context).size.width,
+              padding:
+                  EdgeInsets.only(top: 75, bottom: 10, right: 30, left: 30),
+              decoration: BoxDecoration(
+                color: Color(0xFF282828),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(35.0),
+                  bottomRight: Radius.circular(35.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.7),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
               child: Column(
-                  children : [Container(
-
-                    height: MediaQuery.of(context).size.height * 0.32,
-                    width: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.only(top: 75, bottom: 10, right: 30, left: 30),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF282828),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(35.0),
-                        bottomRight: Radius.circular(35.0),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.7),
-                          spreadRadius: 5,
-                          blurRadius: 5,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Hello, ${userData['username']}",
-                          style: TextStyle(
-                            color: Color(0xFFD75912),
-                            fontSize: 30,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        SizedBox(height: 10), // Add spacing between the texts
-                        Text(
-                          "What would you like to cook today?",
-                          style: TextStyle(
-                            color: Color(0xFFE3E3E3),
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        ),
-                        SizedBox(height: 15,),
-                        Container(
-                          decoration: BoxDecoration(
-                            color : Color(0xFFE3E3E3),
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          child: DropDownTextField(
-                          buttonText: "Search",
-                          textEditingController: TextEditingController(),
-                          hint: 'Select Ingredients',
-                          options: {1:"Tomato",2:"Patato",3:"Chicken",4:"Pasta",5:"Pesto Sauce",6:"Arrabiata Sauce",7:"Filler",8:"Filler",9:"Filler",10:"Filler",11:"Filler"},
-                          selectedOptions: [2,3],
-                          onChanged: (selectedIds) {
-                            //setState(() => selectedIds);
-                          },
-                          multiple: true,
-                        ),
-                        )
-                      ],
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello, ${userData['username']}",
+                    style: TextStyle(
+                      color: Color(0xFFD75912),
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
-                    SizedBox(height: 10),
-                    FoodBox(id: "x0KwRvsB0sHvGPZpieKe"),
-                    // FoodBox(),
-                    // FoodBox(),
-                    // FoodBox(),
-                    // FoodBox(),
-
-                  ])
-          )
-      ),
+                  SizedBox(height: 10), // Add spacing between the texts
+                  Text(
+                    "What would you like to cook today?",
+                    style: TextStyle(
+                      color: Color(0xFFE3E3E3),
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFE3E3E3),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: DropDownTextField(
+                      buttonText: "Search",
+                      textEditingController: TextEditingController(),
+                      hint: 'Select Ingredients',
+                      options: {
+                        1: "Tomato",
+                        2: "Patato",
+                        3: "Chicken",
+                        4: "Pasta",
+                        5: "Pesto Sauce",
+                        6: "Arrabiata Sauce",
+                        7: "Filler",
+                        8: "Filler",
+                        9: "Filler",
+                        10: "Filler",
+                        11: "Filler"
+                      },
+                      selectedOptions: [2, 3],
+                      onChanged: (selectedIds) {
+                        //setState(() => selectedIds);
+                      },
+                      multiple: true,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            FoodBox(id: suggestedRecipeIds[0]),
+            FoodBox(id: suggestedRecipeIds[1]),
+            // FoodBox(),
+            // FoodBox(),
+            // FoodBox(),
+          ]))),
     );
   }
 }
---
+++ bastirchef/lib/services/suggestion_service.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class SuggestionService {
+  final String _baseUrl =
+      'https://us-central1-bastirchef-3aeef.cloudfunctions.net/makeSuggestion';
+
+  Future<List<dynamic>> makeSuggestion(String userId) async {
+    final uri = Uri.parse('$_baseUrl?userId=$userId');
+    try {
+      final response = await http.get(uri);
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body) as Map<String, dynamic>;
+        List<dynamic> recipeSuggestions = jsonResponse['suggestedRecipes'];
+        return recipeSuggestions;
+      } else {
+        print('Failed to load suggestions: ${response.body}');
+      }
+    } catch (e) {
+      print('Caught error: $e');
+    }
+    return [];
+  }
+}
