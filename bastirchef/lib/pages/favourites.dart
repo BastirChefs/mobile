fe7a21f6e94d6fee53931daa89523144f61715b6
Hızır Emirhan Keskin <keskinh19@itu.edu.tr>
2023-12-30 15:33:33 +0300

    favourite recipes are listed in favourites page

--- bastirchef/lib/pages/favourites.dart
+++ bastirchef/lib/pages/favourites.dart
@@ -1,12 +1,56 @@
 import 'package:bastirchef/firebase_options.dart';
 import 'package:bastirchef/pages/src/food_box.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:bastirchef/resources/auth_methods.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'src/drop_down_text_field.dart';
 
 
-class Favourites extends StatelessWidget {
+class Favourites extends StatefulWidget {
 
+  @override
+  State<Favourites> createState() => _FavouritesState();
+}
+
+class _FavouritesState extends State<Favourites> {
+  var userData = {};
+  bool isLoading = false;
+  List<dynamic> favourites = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .get();
+
+      userData = userSnap.data()!;
+      print(userData);
+      
+      if (userData.containsKey('favorite_recipes')) {
+        favourites = userData['favorite_recipes'];
+        print(favourites);
+      }
+      setState(() {});
+    } catch (e) {
+      print(e);
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,12 +78,7 @@ class Favourites extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children : [
                     SizedBox(height: 30),
-                    // FoodBox(),
-                    // FoodBox(),
-                    // FoodBox(),
-                    // FoodBox(),
-                    // FoodBox(),
-
+                    for(var id in favourites) FoodBox(id: id),
                   ]))
           )
       ),
