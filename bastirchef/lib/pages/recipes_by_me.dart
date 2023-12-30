b35fab6f601df15880cbc9079fbc8657c908eccb
Hızır Emirhan Keskin <keskinh19@itu.edu.tr>
2023-12-30 19:34:17 +0300

    added user recipes display and remove functions

--- bastirchef/lib/pages/recipes_by_me.dart
+++ bastirchef/lib/pages/recipes_by_me.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'src/button.dart';
+import 'package:bastirchef/pages/src/food_box.dart';
 
 class RecipesByMe extends StatefulWidget {
   @override
@@ -9,6 +12,73 @@ class RecipesByMe extends StatefulWidget {
 }
 
 class _RecipesByMeState extends State<RecipesByMe> {
+  var userData = {};
+  bool isLoading = false;
+  List userRecipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .get();
+
+      userData = userSnap.data()!;
+      print(userData);
+
+      userRecipes = userData['recipes'];
+      
+    } catch (e) {
+      print(e);
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  update() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .update({'recipes': userRecipes});
+    } catch (e) {
+      print(e);
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  deleteRecipe(id) async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      await FirebaseFirestore.instance
+          .collection('recipes')
+          .doc(id)
+          .delete();
+    } catch (e) {
+      print(e);
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -42,8 +112,19 @@ class _RecipesByMeState extends State<RecipesByMe> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children : [
                     Text("hello"),
-                    Text("recipes by me will be here")
-
+                    Text("recipes by me will be here"),
+                    for(var id in userRecipes) Column(
+                      children: [
+                        FoodBox(id: id),
+                        GestureDetector(
+                          onTap: () {
+                            userRecipes.removeAt(userRecipes.indexOf(id));
+                            deleteRecipe(id);
+                            update();
+                          },
+                          child: Text("Remove", style: TextStyle(color: Colors.red, fontSize: 12.0),)
+                        )
+                      ],),
                   ]))
           )
       ),
