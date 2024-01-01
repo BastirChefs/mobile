12a580d8d5865a2ac1d2aa925cfb8f629c25a736
Hızır Emirhan Keskin <keskinh19@itu.edu.tr>
2024-01-01 12:51:29 +0300

    recipe display related functions are implemented
    adding to favorites
    adding to a recipe list
    adding a comment
    checking missing ingredients

--- bastirchef/lib/pages/recipe_display.dart
+++ bastirchef/lib/pages/recipe_display.dart
@@ -13,8 +13,10 @@ class RecipeDisplay extends StatefulWidget {
 }
 
   class _RecipeDisplayState extends State<RecipeDisplay> {
-    var recipeData= {};
+    var recipeData = {};
+    var userData = {};
     bool isLoading = false;
+    var username;
 
     @override
     void initState() {
@@ -27,6 +29,14 @@ class RecipeDisplay extends StatefulWidget {
         isLoading = true;
       });
       try {
+        var userSnap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .get();
+
+        userData = userSnap.data()!;
+        username = userData['username'];
+
         var recipeSnap = await FirebaseFirestore.instance
             .collection('recipes')
             .doc(widget.id)
@@ -38,6 +48,99 @@ class RecipeDisplay extends StatefulWidget {
       } catch (e) {
         print(e);
       }
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    addToFav() async {
+      setState(() {
+        isLoading = true;
+      });
+      var currentLikes = recipeData['reactions']['delicios'];
+      try {
+        await FirebaseFirestore.instance
+          .collection('recipes')
+          .doc(widget.id)
+          .update({'reactions.delicios': currentLikes+1});
+
+        await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set({'favorite_recipes': widget.id}, SetOptions(merge: true));
+      } catch (e) {
+        print(e);
+      }
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    addComment(comment) async {
+      setState(() {
+        isLoading = true;
+      });
+      try {
+        Map commentMap = {
+          'comment': comment,
+          'createdAt': DateTime.now(),
+          'userId': username,
+        };
+        print(commentMap);
+
+        await FirebaseFirestore.instance
+          .collection('recipes')
+          .doc(widget.id)
+          .set({'comments': commentMap}, SetOptions(merge: true));
+      } catch (e) {
+        print(e);
+      }
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    List checkMissingIngredients() {
+      setState(() {
+        isLoading = true;
+      });
+      List missingItems = []; 
+      var keyList = recipeData['ingredients'].keys;
+      for(var keys in keyList){
+        var ingredient = recipeData['ingredients'][keys];
+        if(userData['storage'][keys] == null){
+          var key = ingredient;
+          var value = ingredient['amount'];
+          Map item = {key: value};
+          missingItems.add(item);
+        }
+        else if(userData['storage'][keys] < ingredient['amount']){
+          var key = ingredient;
+          var value = ingredient['amount'] - userData['storage'][keys];
+          Map item = {key: value};
+          missingItems.add(item);
+        }
+      }
+      setState(() {
+        isLoading = false;
+      });
+      return missingItems;
+    }
+
+    addToList(listName) async {
+      setState(() {
+        isLoading = true;
+      });
+      try {
+        var addedList = userData['recipe_list'][listName].add(widget.id);
+        await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .update({'recipe_list.$listName': addedList});
+      } catch (e) {
+        print(e);
+      }
+
       setState(() {
         isLoading = false;
       });
